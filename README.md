fa6883aedfa819ae17e63c8b22bc206d113660f9
QANstar <1435385924@qq.com>
2022-03-20 16:25:02 +0800

    添加说明和element

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-# doublezero-official
+# 双零团队官网
 
-## Project setup
+## 相关插件
+
+- [Element Ui](https://element-plus.org/zh-CN/)
+
+## 起步
+
+```
+git clone git@github.com:QANstar/DoubleZero-Official.git
+cd doublezero-official
+npm install
+npm run serve
+```
+
+## 同步 git
+
+```
+git pull
+```
+
+## 提交更改
+
+```
+git add .
+git commit -m "你的说明"
+git push origin main
 ```
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+    <el-button>中文</el-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
